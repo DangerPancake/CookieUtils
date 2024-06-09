cc784e06c987886fb4c885339196b1bbeec01137
DangerPancake <145292160+DangerPancake@users.noreply.github.com>
2024-06-09 13:04:43 -0600

    ping finished?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import RenderLib from "../RenderLib/index.js";
 import settings from "./settings";
 import "./functions.js";
+import { Vector, deleteWaypoint } from "./functions.js";
 
 //all variables are defined here in order to limit the chances of a memory leak occuring
 let testBlock = null;
@@ -15,26 +16,33 @@ let temp = 0;
 let lastArea = null;
 let counter = 0;
 
+// Waypoint vars
+let [width, height] = [1, 1];
+let [red, green, blue, alpha] = [0, 0, 1, 1];
+let phase = true;
+
 //renders pings
 register('renderWorld', () => {
     for (let i = 0; i < pingList.length; i++) {
-
-        // TYPE DETECTION: CURRENTLY ONLY WORKS WITH "BOX", "INNERBOX" AND ALSO "BARITONEBOX" I WILL WORK ON IT TOMORROW IM TIRED - ALSO THE WAYPOINT DELETION TIME IS STILL SET TO 1SECOND I WILL FIX TOMORROW!!! //
-        if (pingList[i].getType() == null || pingList[i].getType() == "BOX") {
-            RenderLib.drawEspBox(Math.floor(pingList[i].getX()) + 0.5, Math.floor(pingList[i].getY()), Math.floor(pingList[i].getZ()) + 0.5, 1, 1, 0, 0, 1, 1, true);
-        } else if (pingList[i].getType() == "INNERBOX") {
-            RenderLib.drawInnerEspBox(Math.floor(pingList[i].getX()) + 0.5, Math.floor(pingList[i].getY()), Math.floor(pingList[i].getZ()) + 0.5, 1, 1, 0, 0, 1, 1, true);
-        } else if (pingList[i].getType() == "BARITONEBOX") {
-            RenderLib.drawBaritoneEspBox(Math.floor(pingList[i].getX()) + 0.5, Math.floor(pingList[i].getY()), Math.floor(pingList[i].getZ()) + 0.5, 1, 1, 0, 0, 1, 1, true);
+        let wp = pingList[i];
+        let [wptype, wptext] = [wp.getType(), wp.getText()];
+        let [wpx, wpy, wpz] = [Math.floor(wp.getX()), Math.floor(wp.getY()), Math.floor(wp.getZ())];
+        // draw Waypoint
+        if (wptype == null || wptype == "BOX") {
+            RenderLib.drawEspBox(wpx + 0.5, wpy, wpz + 0.5, width, height, red, green, blue, alpha, phase);
+        } else if (wptype == "INNERBOX") {
+            RenderLib.drawInnerEspBox(wpx + 0.5, wpy, wpz + 0.5, width, height, red, green, blue, alpha, phase);
+        } else if (wptype == "BARITONEBOX") {
+            RenderLib.drawBaritoneEspBox(wpx, wpy, wpz, width, height, red, green, blue, alpha, phase);
         }
-        // RenderLib.drawInnerEspBox(Math.floor(pingList[i].getX()) + 0.5, Math.floor(pingList[i].getY()), Math.floor(pingList[i].getZ()) + 0.5, 1, 1, 0, 0, 1, 1, true);
-        if (pingList[i].getText() == null || pingList[i].getText() == "null") {
-            Tessellator.drawString(pingList[i].getName(), Math.floor(pingList[i].getX()) + 0.5, Math.floor(pingList[i].getY()) + 1, Math.floor(pingList[i].getZ()) + 0.5);
+
+        // draw Waypoint Text
+        if (wptext == null || wptext == "null") {
+            Tessellator.drawString(wp.getName(), wpx + 0.5, wpy + 1, wpz + 0.5);
         } else {
-            Tessellator.drawString(pingList[i].getText(), Math.floor(pingList[i].getX()) + 0.5, Math.floor(pingList[i].getY()) + 1, Math.floor(pingList[i].getZ()) + 0.5);
+            Tessellator.drawString(wptext, wpx + 0.5, wpy + 1, wpz + 0.5);
         }
     }
-
 });
 
 //called every tick
@@ -55,6 +63,10 @@ register("command", (user) => {
     settings.openGUI();
 }).setName("cu").setAliases("cookieutils"); 
 
+register("command", (user) => {
+    pingList = [];
+}).setName("clearpings").setAliases("cp"); 
+
 //creates a new ping depending on where the player is looking and posts it in chat
 register("command", (user, text, type) => {
     if (user != null) {
@@ -68,17 +80,21 @@ register("command", (user, text, type) => {
     }
     
     if (text == null) {
-        text = "null";
+        if (settings.pingText != null && settings.pingText != "") {
+            text = settings.pingText;
+        } else {
+            text = "null";
+        }
+        
     }
 
     if (type == null) {
-        switch (settings.pingShape) {
-            case 1:
-                type = "INNERBOX";
-            case 2:
-                type = "BARITONEBOX";
-            case 0:
-                type = "BOX";
+        if (settings.pingShape == 0) {
+            type = "BOX";
+        } else if (settings.pingShape == 1) {
+            type = "INNERBOX";
+        } else {
+            type = "BARITONEBOX";
         }
     }
 
@@ -100,7 +116,7 @@ register("command", (user, text, type) => {
     //posting in chat
     if (testBlock.getY() < 300 && testBlock.getY() > 0) {
         ChatLib.chat("Block at: " + Math.floor(testBlock.getX()) + ", " + Math.floor(testBlock.getY()) + ", " + Math.floor(testBlock.getZ()) + " is: " + World.getBlockAt(Math.floor(testBlock.getX()), Math.floor(testBlock.getY()), Math.floor(testBlock.getZ())).getType().getName());
-        ChatLib.command("tell sweatykeycaps " + "x: " + Math.floor(testBlock.getX()) + ", y: " + Math.floor(testBlock.getY()) + ", z: " + Math.floor(testBlock.getZ()) + ", t: " + text + ", t:" + type + ". Generated using Cookie Utils /cu");
+        ChatLib.command("pc " + "x: " + Math.floor(testBlock.getX()) + ", y: " + Math.floor(testBlock.getY()) + ", z: " + Math.floor(testBlock.getZ()) + " t: " + text + ", t:" + type + ". Generated using Cookie Utils /cu");
     } else {
         ChatLib.chat("No block found within maximum height.");
     }
@@ -109,7 +125,6 @@ register("command", (user, text, type) => {
 
 //fetches waypoints from other users
 register("chat", (player, x, y, z, text, type, event) => {
-    ChatLib.chat("Ping fetched from " + player);
     if (/to(?:\s+\w+)+:/i.test(player)) {
         player = Player.getName();
     } else {
@@ -117,7 +132,18 @@ register("chat", (player, x, y, z, text, type, event) => {
     }
     temp = 0;
     for (let i = 0; i < pingList.length; i++) {
-        if (pingList[i].getName() == player) {
+        if (pingList[i].getName() == player && settings.onePing) {
+            pingList.splice(i, 1);
+            pingList.push(new Vector(x, y, z, player, text, type));
+
+            if (settings.delete_waypoint_after != 0) {
+                setTimeout(() => {
+                    pingList.splice(pingList.indexOf(r));
+                }, settings.delete_waypoint_after);
+            }
+            temp = 1;
+            break;
+        } else if (pingList[i].getText() == text && settings.onePingText) {
             pingList.splice(i, 1);
             pingList.push(new Vector(x, y, z, player, text, type));
 
@@ -139,70 +165,9 @@ register("chat", (player, x, y, z, text, type, event) => {
             }, settings.delete_waypoint_after);
         }
     }
-}).setCriteria("${player}: x: ${x}, y: ${y}, z: ${z}, t: ${text}, t:${type}. Generated using Cookie Utils /cu").setContains();
+    ChatLib.chat("Ping fetched from " + player);
+}).setCriteria("${player}: x: ${x}, y: ${y}, z: ${z} t: ${text}, t:${type}. Generated using Cookie Utils /cu").setContains();
 
-
-//vector class
-class Vector {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    constructor(x, y, z, name) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.name = name;
-    }
-
-    constructor(x, y, z, name, text) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.name = name;
-        this.text = text;
-    }
-
-    constructor(x, y, z, name, text, type) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.name = name;
-        this.text = text;
-        this.type = type;
-    }
-
-    getX() {
-        return this.x;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    getY() {
-        return this.y;
-    }
-
-    getZ() {
-        return this.z;
-    }
-
-    getText() {
-        return this.text;
-    }
-
-    getType() {
-        return this.type;
-    }
-
-    add(vector) {
-        this.x += vector.getX()
-        this.y += vector.getY()
-        this.z += vector.getZ()
-    }
-}
 /*
 References
 tablist = TabList?.getNames()?.map(name => name?.removeFormatting());
